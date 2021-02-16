6055ca7a87bffd63d786b55f87192f5a89995eaa
Lukas Jacomin <lukasjacomin@gmail.com>
2021-02-16 12:28:09 -0300

    Docker scripts EOL

--- docker/scripts/start-app.sh
+++ docker/scripts/start-app.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 cp /var/www/docker/.env.docker /var/www/.env
 chown www-data:www-data -R storage/
 /opt/wait-for-it.sh transaction-mysql:3306 -t 60 -- php artisan migrate --force --seed
