8466bbccf4f1c3c4a9188ae1443222848008b7b6
Lukas Jacomin <lukasjacomin@gmail.com>
2021-02-16 00:58:35 -0300

    Table indexes

--- database/migrations/2021_02_12_205431_create_transactions_table.php
+++ database/migrations/2021_02_12_205431_create_transactions_table.php
@@ -15,14 +15,14 @@ class CreateTransactionsTable extends Migration {
          $table->id();
          $table->unsignedInteger('transaction_type_id')->nullable(false);
          $table->unsignedBigInteger('amount')->nullable(false)->comment('Integer rather than float due to float round errors.');
-         $table->unsignedBigInteger('user_id')->nullable(false)->comment('Transaction main user. On tranfers between users he is the \'from\' user.');
+         $table->unsignedBigInteger('user_id')->nullable(false)->index()->comment('Transaction main user. On tranfers between users he is the \'from\' user.');
          $table->unsignedBigInteger('user_id_ref')->nullable(true)->comment('Transaction \'to\' user.');
          $table->string('description', 500)->nullable()->comment('Transaction description');
-         $table->unsignedBigInteger('transaction_id_ref')->nullable(true)->comment('Transaction reference ID.');
-         $table->tinyInteger('status')->default(0)->comment('Transaction status');
+         $table->unsignedBigInteger('transaction_id_ref')->index()->nullable(true)->comment('Transaction reference ID.');
+         $table->tinyInteger('status')->default(0)->index()->comment('Transaction status');
          $table->string('error', 500)->nullable()->comment('Transaction error message');
 
-         $table->foreign('transaction_type_id')->references('id')->on('transaction_types');
+         $table->foreign('transaction_type_id')->index()->references('id')->on('transaction_types');
          $table->foreign('user_id')->references('id')->on('users');
          $table->foreign('user_id_ref')->references('id')->on('users');
          $table->foreign('transaction_id_ref')->references('id')->on('transactions');
